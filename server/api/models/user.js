627dd3872d17d2baeadab2388381af926ad4ce51
MYHG <myhairguide@gmail.com>
2020-05-11 18:37:46 +0100

    Add hairQuizSchema to user model

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -2,9 +2,49 @@ const mongoose = require("mongoose");
 const uniqueValidator = require('mongoose-unique-validator');
 
 mongoose.Promise = global.Promise;
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const hairQuizSchema = new mongoose.Schema({
+    texture: {
+        description: { type: String },
+        natural: { type: Boolean },
+        chemicallyProcessed: { type: Boolean },
+        transitioning: { type: Boolean }
+    },
+    porosity: {
+        description: { type: String },
+        low: { type: Boolean },
+        normal: { type: Boolean  },
+        high: { type: Boolean }
+    },
+    thickness: {
+        description: { type: String },
+        thin: { type: Boolean },
+        thick: { type: Boolean }
+    },
+    response: {
+        description: { type: String },
+        oneToTwo: { type: Boolean },
+        twoToThree: { type: Boolean },
+        fourToFive: { type: Boolean },
+        onceAWeek: { type: Boolean }
+    },
+    hairProblems: {
+        description: { type: String },
+        dry: { type: Boolean },
+        breakage: { type: Boolean },
+        thinningEdges: { type: Boolean },
+        splitEnds: { type: Boolean },
+        excessiveDandruff: { type: Boolean },
+        tangling: { type: Boolean },
+        heatDamage: { type: Boolean },
+        limpHair: { type: Boolean },
+        frizzy: { type: Boolean },
+        colourMaintenance: { type: Boolean },
+        curlDefinition: { type: Boolean }
+    }
+});
+
+const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: true,
@@ -25,61 +65,10 @@ const userSchema = new Schema({
     age: {
         type: Number,
         required: true
-    },
-    hairQuizResponses: [{ type: Schema.Types.ObjectId, ref: 'HairQuiz ' }]
-});
-
-const hairQuizSchema = new Schema({
-    texture: {
-        natural: {
-            type: Boolean
-        },
-        chemicallyProcessed: {
-            type: Boolean
-        },
-        transitioning: {
-            type: Boolean
-        }
-    },
-    hairProblems: {
-        dry: {
-            type: Boolean
-        },
-        breakage: {
-            type: Boolean
-        },
-        thinningEdges: {
-            type: Boolean
-        },
-        splitEnds: {
-            type: Boolean
-        },
-        excessiveDandruff: {
-            type: Boolean
-        },
-        tangling: {
-            type: Boolean
-        },
-        heatDamage: {
-            type: Boolean
-        },
-        limpHair: {
-            type: Boolean
-        },
-        frizzy: {
-            type: Boolean
-        },
-        colourMaintenance: {
-            type: Boolean
-        },
-        curlDefinition: {
-            type: Boolean
-        }
-    },
+    }, 
+    hairQuizResponses: [hairQuizSchema]
 });
 
 
 userSchema.plugin(uniqueValidator, { message: 'already exists.' });
-
-mongoose.model('HairQuiz', hairQuizSchema);
-mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema), mongoose.model('HairQuiz', hairQuizSchema);
